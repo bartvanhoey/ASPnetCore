94b37000fee656778d6fa350770949f37162151b
Bart Van Hoey <bart.vanhoey@euram.eu>
2019-02-26 09:47:07 +0100

    end part11

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement
 {
@@ -23,18 +24,35 @@ namespace EmployeeManagement
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW1: Incoming request");
+                await next();
+                logger.LogInformation("MW1: Outgoing response");
+
+            });
+            
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW2: Incoming request");
+                await next();
+                logger.LogInformation("MW2: Outgoing response");
+
+            });
+            
             app.Run(async (context) =>
             {
-                //await context.Response.WriteAsync("Hello World!");
-                //await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
-                await context.Response.WriteAsync(_configuration["MyKey"]);
+                
+
+                await context.Response.WriteAsync("MW3: Request handled and response produced");
+                logger.LogInformation("MW3: Request handled  and response produced");
             });
         }
     }
