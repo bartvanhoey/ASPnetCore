703c92cc94f511e55f895b25a5fdb3ccad748915
Bart Van Hoey <bart.vanhoey@euram.eu>
2019-02-27 09:42:28 +0100

    end part 16

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+#region License
+// HomeController.cs - Created 2019-02-27 9:17 
+// All code Copyright (c) 2019-2019 and property of EurAm plc
+// Reproduction of this material is strictly forbidden unless prior written permission is obtained from EurAm plc
+#endregion
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello from Index";
+        }
+        
+    }
+}
--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -21,24 +21,17 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                var developerExceptionPageOptions = new DeveloperExceptionPageOptions {SourceCodeLineCount = 10};
-                app.UseDeveloperExceptionPage(developerExceptionPageOptions );
-            }
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
-            app.UseFileServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
-            app.Run(async (context) =>
-            {
-                throw new Exception("Some error processing the request");
-                await context.Response.WriteAsync("Hello World");
-            });
         }
     }
 }
